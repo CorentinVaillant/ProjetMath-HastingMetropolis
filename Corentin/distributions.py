73ec2a37249eb5ced04c270c8dbd10424aa38918
Corentin <c.vaillant848@gmail.com>
2025-03-10 14:59:27 +0100

    ajouts de lois, TODO : animation de l'algo

--- Corentin/distributions.py
+++ Corentin/distributions.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+from math import factorial, comb
+
 pi = np.pi
 exp = np.exp
 
-def norm_dist(mean,variance):
-    return lambda x: (1/(np.sqrt(2*pi*variance))) * exp(- ((x-mean)**2)/(2*variance) )
+
+
+def poisson(l):
+    return lambda k: ((l**k)/(factorial(k)))*np.exp(-l)
+
+def binomial(n,p):
+    return lambda k: comb(n,k) * p**k * (1-p)**(n-k)
+
+def geo(p):
+    return lambda k: p*((1-p)**(k-1))
